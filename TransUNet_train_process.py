ead9b00929d40fcd8c4ee20a35a634dc4710fb02
ambroiseb <43398946+ambroiseb@users.noreply.github.com>
2021-05-11 17:34:48 +0200

    Initial commit

---
+++ TransUNet_train_process.py
@@ -0,0 +1,216 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import core, dataprocess
+import copy
+# Your imports below
+from ikomia.dnn import datasetio
+from ikomia.dnn import dnntrain
+import os
+import utils
+from pathlib import Path
+import numpy as np
+from networks.vit_seg_modeling import VisionTransformer as ViT_seg
+from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+# --------------------
+# - Class to handle the process parameters
+# - Inherits PyCore.CProtocolTaskParam from Ikomia API
+# --------------------
+class TransUNet_TrainParam(dnntrain.TrainParam):
+
+    def __init__(self):
+        dnntrain.TrainParam.__init__(self)
+        # Place default value initialization here
+        # Example : self.windowSize = 25
+        self.cfg["modelName"]= "TransUNet"
+        self.cfg["inputSize"] = 256
+        self.cfg["patchSize"] = 16
+        self.cfg["maxIter"] = 1000
+        self.cfg["batchSize"] = 1
+        self.cfg["splitTrainTest"] = 90
+        self.cfg["evalPeriod"] = 100
+        self.cfg["pretrain"] = True
+        self.cfg["outputFolder"] = ""
+        self.cfg["baseLearningRate"] = 0.01
+        self.cfg["pretrain"] = True
+
+    def setParamMap(self, paramMap):
+        # Set parameters values from Ikomia application
+        # Parameters values are stored as string and accessible like a python dict
+        # Example : self.windowSize = int(paramMap["windowSize"])
+        self.cfg["inputSize"] = int(paramMap["inputSize"])
+        self.cfg["patchSize"] = int(paramMap["patchSize"])
+        self.cfg["maxIter"] = int(paramMap["maxIter"])
+        self.cfg["batchSize"] = int(paramMap["batchSize"])
+        self.cfg["splitTrainTest"] = int(paramMap["splitTrainTest"])
+        self.cfg["evalPeriod"] = int(paramMap["evalPeriod"])
+        self.cfg["pretrain"] = bool(paramMap["pretrain"])
+        self.cfg["outputFolder"] = paramMap["outputFolder"]
+        self.cfg["baseLearningRate"] = float(paramMap["baseLearningRate"])
+        self.cfg["pretrain"] = bool(paramMap["pretrain"])
+
+# --------------------
+# - Class which implements the process
+# - Inherits PyCore.CProtocolTask or derived from Ikomia API
+# --------------------
+class TransUNet_TrainProcess(dnntrain.TrainProcess):
+
+    def __init__(self, name, param):
+        dnntrain.TrainProcess.__init__(self, name,param)
+        # Add input/output of the process here
+        self.addInput(datasetio.IkDatasetIO())
+        self.stop_train=False
+        # Create parameters class
+        if param is None:
+            self.setParam(TransUNet_TrainParam())
+        else:
+            self.setParam(copy.deepcopy(param))
+
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        param = self.getParam()
+        if param is not None:
+            return param.cfg["maxIter"]
+        else:
+            return 1
+
+    def run(self):
+        # Core function of your process
+        # Call beginTaskRun for initialization
+        self.beginTaskRun()
+
+        self.stop_train = False
+        dir_path = os.path.dirname(__file__)
+        pretrained_path= Path(dir_path+"/networks/"+"R50+ViT-B_16.npz")
+        if not(os.path.isfile(pretrained_path)):
+            import requests
+            print('Downloading weights')
+
+            url = 'https://storage.googleapis.com/vit_models/imagenet21k%2Bimagenet2012/R50%2BViT-B_16.npz'
+            response = requests.get(url, stream=True)
+            total_size_in_bytes = int(response.headers.get('content-length', 0))
+            block_size = 1024  # 1 Kibibyte
+            progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+            with open(pretrained_path, 'wb') as file:
+                for data in response.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    file.write(data)
+            progress_bar.close()
+            if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+                print("ERROR, something went wrong")
+
+            print('Weights downloaded')
+
+        input = self.getInput(0)
+
+        # Get parameters :
+        param = self.getParam()
+        batch_size = param.cfg["batchSize"]
+        img_size = param.cfg["inputSize"]
+        max_iter = param.cfg["maxIter"]
+        split_train_test = param.cfg["splitTrainTest"]/100
+        eval_period = param.cfg["evalPeriod"]
+        pretrain = param.cfg["pretrain"]
+        base_lr = param.cfg["baseLearningRate"]
+        patch_size = param.cfg["patchSize"]
+        if len(input.data) > 0:
+            str_datetime = datetime.now().strftime("%d-%m-%YT%Hh%Mm%Ss")
+            output_path = os.path.join(dir_path,"output",str_datetime)
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+            config_vit = CONFIGS_ViT_seg['R50-ViT-B_16']
+
+            if not(input.has_bckgnd_class):
+                tmp_dict = {0:"background"}
+                for k,name in input.data["metadata"]["category_names"].items():
+                    tmp_dict[k+1]=name
+                input.data["metadata"]["category_names"] = tmp_dict
+                input.has_bckgnd_class = True
+            num_classes = len(input.data["metadata"]["category_names"])
+
+            config_vit.n_classes = num_classes
+            config_vit.n_skip = 3
+            config_vit.patches_size = patch_size
+            config_vit.patches.grid = (int(img_size / config_vit.patches_size), int(img_size / config_vit.patches_size))
+            config_vit.class_names = [name for k,name in input.data["metadata"]["category_names"].items()]
+            model = ViT_seg(config_vit, img_size=img_size, num_classes=config_vit.n_classes).cuda()
+
+            if pretrain == True:
+                with np.load(pretrained_path) as data:
+                    pretrained_names = data.files
+                    pretrained_dict = {}
+                    for i, name in enumerate(pretrained_names):
+                        pretrained_dict[Path(name)] = data[pretrained_names[i]]
+
+                model.load_from(weights=pretrained_dict)
+
+            tb_logdir = Path(self.getTensorboardLogDir()+"/"+param.cfg["modelName"]+"/"+str_datetime)
+            tb_writer = SummaryWriter(tb_logdir)
+            utils.my_trainer(model, batch_size, img_size, num_classes, base_lr, max_iter, output_path, input.data, pretrain,
+                       split_train_test, eval_period,self.get_stop,self.emitStepProgress,tb_writer)
+            with open(os.path.join(output_path,"config.yaml"), 'w') as fp:
+                fp.write(config_vit.to_yaml())
+
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+    def get_stop(self):
+        return self.stop_train
+
+    def stop(self):
+        super().stop()
+        self.stop_train=True
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CProcessFactory from Ikomia API
+# --------------------
+class TransUNet_TrainProcessFactory(dataprocess.CProcessFactory):
+
+    def __init__(self):
+        dataprocess.CProcessFactory.__init__(self)
+        # Set process information as string here
+        self.info.name = "TransUNet_Train"
+        self.info.shortDescription = "your short description"
+        self.info.description = "your description"
+        self.info.authors = "Plugin authors"
+        # relative path -> as displayed in Ikomia application process tree
+        self.info.path = "Plugins/Python"
+        self.info.version = "1.0.0"
+        # self.info.iconPath = "your path to a specific icon"
+        self.info.authors = "algorithm author"
+        self.info.article = "title of associated research article"
+        self.info.journal = "publication journal"
+        self.info.year = 2021
+        self.info.license = "MIT License"
+        # URL of documentation
+        self.info.documentationLink = ""
+        # Code source repository
+        self.info.repository = ""
+        # Keywords used for search
+        self.info.keywords = "your,keywords,here"
+
+    def create(self, param=None):
+        # Create process object
+        return TransUNet_TrainProcess(self.info.name, param)
---
+++ TransUNet_train_widget.py
@@ -0,0 +1,144 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import utils, core, dataprocess
+import TransUNet_Train_process as processMod
+
+#PyQt GUI framework
+from PyQt5.QtWidgets import *
+
+
+# --------------------
+# - Class which implements widget associated with the process
+# - Inherits PyCore.CProtocolTaskWidget from Ikomia API
+# --------------------
+class TransUNet_TrainWidget(core.CProtocolTaskWidget):
+
+    def __init__(self, param, parent):
+        core.CProtocolTaskWidget.__init__(self, parent)
+
+        if param is None:
+            self.parameters = processMod.TransUNet_TrainParam()
+        else:
+            self.parameters = param
+
+        # Create layout : QGridLayout by default
+        self.gridLayout = QGridLayout()
+        # PyQt -> Qt wrapping
+        layout_ptr = utils.PyQtToQt(self.gridLayout)
+
+        inputSizeLabel = QLabel("Desired input size:")
+        self.inputSizeSpinBox = QSpinBox()
+        self.inputSizeSpinBox.setRange(16, 4096)
+        self.inputSizeSpinBox.setSingleStep(1)
+        self.inputSizeSpinBox.setValue(self.parameters.cfg["inputSize"])
+
+        maxIterLabel = QLabel("Max iter:")
+        self.maxIterSpinBox = QSpinBox()
+        self.maxIterSpinBox.setRange(0, 2147483647)
+        self.maxIterSpinBox.setSingleStep(1)
+        self.maxIterSpinBox.setValue(self.parameters.cfg["maxIter"])
+
+        batchSizeLabel = QLabel("Batch size:")
+        self.batchSizeSpinBox = QSpinBox()
+        self.batchSizeSpinBox.setRange(1, 2147483647)
+        self.batchSizeSpinBox.setSingleStep(1)
+        self.batchSizeSpinBox.setValue(self.parameters.cfg["batchSize"])
+
+        splitTrainTestLabel = QLabel("Train test percentage:")
+        self.splitTrainTestSpinBox = QSpinBox()
+        self.splitTrainTestSpinBox.setRange(0, 100)
+        self.splitTrainTestSpinBox.setSingleStep(1)
+        self.splitTrainTestSpinBox.setValue(self.parameters.cfg["splitTrainTest"])
+
+        evalPeriodLabel = QLabel("Evaluation period:")
+        self.evalPeriodSpinBox = QSpinBox()
+        self.evalPeriodSpinBox.setRange(0, 2147483647)
+        self.evalPeriodSpinBox.setSingleStep(1)
+        self.evalPeriodSpinBox.setValue(self.parameters.cfg["evalPeriod"])
+
+        baseLearningRateLabel = QLabel("Base learning rate:")
+        self.baseLearningRateSpinBox = QDoubleSpinBox()
+        self.baseLearningRateSpinBox.setRange(0, 10)
+        self.baseLearningRateSpinBox.setDecimals(4)
+        self.baseLearningRateSpinBox.setSingleStep(0.0001)
+        self.baseLearningRateSpinBox.setValue(self.parameters.cfg["baseLearningRate"])
+
+        patchSizeLabel = QLabel("Patch size:")
+        self.patchSizeLabelSpinBox = QSpinBox()
+        self.patchSizeLabelSpinBox.setRange(1,64)
+        self.patchSizeLabelSpinBox.setSingleStep(1)
+        self.patchSizeLabelSpinBox.setValue(self.parameters.cfg["patchSize"])
+
+
+        # Set widget layout
+
+        self.gridLayout.addWidget(inputSizeLabel, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.inputSizeSpinBox, 0, 1, 1, 2)
+        self.gridLayout.addWidget(maxIterLabel, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.maxIterSpinBox, 1, 1, 1, 2)
+        self.gridLayout.addWidget(batchSizeLabel, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.batchSizeSpinBox, 2, 1, 1, 2)
+        self.gridLayout.addWidget(splitTrainTestLabel, 3, 0, 1, 1)
+        self.gridLayout.addWidget(self.splitTrainTestSpinBox, 3, 1, 1, 2)
+        self.gridLayout.addWidget(evalPeriodLabel, 4, 0, 1, 1)
+        self.gridLayout.addWidget(self.evalPeriodSpinBox, 4, 1, 1, 2)
+        self.gridLayout.addWidget(baseLearningRateLabel, 5, 0, 1, 1)
+        self.gridLayout.addWidget(self.baseLearningRateSpinBox, 5, 1, 1, 2)
+        self.gridLayout.addWidget(patchSizeLabel,6,0,1,1)
+        self.gridLayout.addWidget(self.patchSizeLabelSpinBox,6,1,1,2)
+
+        # Output folder
+        self.browse_out_folder = utils.append_browse_file(self.gridLayout, label="Output folder",
+                                                          path=self.parameters.cfg["outputFolder"],
+                                                          tooltip="Select folder",
+                                                          mode=QFileDialog.Directory)
+
+        self.setLayout(layout_ptr)
+
+    def onApply(self):
+        # Apply button clicked slot
+
+        # Get parameters from widget
+        # Example : self.parameters.windowSize = self.spinWindowSize.value()
+        self.parameters.cfg["inputSize"] = self.inputSizeSpinBox.value()
+        self.parameters.cfg["batch_size"] = self.batchSizeSpinBox.value()
+        self.parameters.cfg["maxIter"] = self.maxIterSpinBox.value()
+        self.parameters.cfg["evalPeriod"] = self.evalPeriodSpinBox.value()
+        self.parameters.cfg["splitTrainTest"] = self.splitTrainTestSpinBox.value()
+        self.parameters.cfg["outputFolder"] = self.browse_out_folder.path
+        self.parameters.cfg["patchSize"] = self.patchSizeLabelSpinBox.value()
+        self.parameters.cfg["baseLearningRate"] = self.baseLearningRateSpinBox.value()
+        # Send signal to launch the process
+        self.emitApply(self.parameters)
+
+
+# --------------------
+# - Factory class to build process widget object
+# - Inherits PyDataProcess.CWidgetFactory from Ikomia API
+# --------------------
+class TransUNet_TrainWidgetFactory(dataprocess.CWidgetFactory):
+
+    def __init__(self):
+        dataprocess.CWidgetFactory.__init__(self)
+        # Set the name of the process -> it must be the same as the one declared in the process factory class
+        self.name = "TransUNet_Train"
+
+    def create(self, param):
+        # Create widget object
+        return TransUNet_TrainWidget(param, None)
