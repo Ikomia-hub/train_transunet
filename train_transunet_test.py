ebd5ba290295634bac13c8a30dceabcb96664ec0
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 10:19:29 +0200

    Fix test file

--- train_transunet_test.py
+++ train_transunet_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task
 from ikomia.utils.tests import run_for_test
 
 logger = logging.getLogger(__name__)
@@ -8,11 +7,11 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train transunet =====")
     input_dataset = t.get_input(0)
-    params = task.get_parameters(t)
-    params["maxIter"] = 10
-    params["batchSize"] = 1
-    params["evalPeriod"] = 5
-    params["splitTrainTest"] = 50
-    task.set_parameters(t, params)
+    params = t.get_parameters()
+    params["maxIter"] = "10"
+    params["batchSize"] = "1"
+    params["evalPeriod"] = "5"
+    params["splitTrainTest"] = "50"
+    t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["semantic_segmentation"]["dataset_coco"])
     yield run_for_test(t)
