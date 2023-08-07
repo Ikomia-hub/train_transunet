f239d190f334acf6c2d6028881b1da2cc95004d1
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 16:52:27 +0200

    Fix test file

--- train_transunet_test.py
+++ train_transunet_test.py
@@ -8,10 +8,10 @@ def test(t, data_dict):
     logger.info("===== Test::train transunet =====")
     input_dataset = t.get_input(0)
     params = t.get_parameters()
-    params["maxIter"] = "10"
-    params["batchSize"] = "1"
-    params["evalPeriod"] = "5"
-    params["splitTrainTest"] = "50"
+    params["max_iter"] = "10"
+    params["batch_size"] = "1"
+    params["eval_period"] = "5"
+    params["dataset_split_ratio"] = "50"
     t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["semantic_segmentation"]["dataset_coco"])
     yield run_for_test(t)
